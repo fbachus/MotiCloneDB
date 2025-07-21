1506f8460ae0ae7ef330e320059ff7ea218dc41a
fbachus <felix.bachstein@gmail.com>
2025-07-21 11:44:54 +0200

    fixup! fix(sql-definitions): refine nullable fields

--- migrations/2025-07-18-143551_02_groups/up.sql
+++ migrations/2025-07-18-143551_02_groups/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS groups (
-    id SERIAL PRIMARY KEY,
-    referral_code VARCHAR(15) UNIQUE,
-    admin INTEGER REFERENCES users (id) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    challenge_deadline TIMESTAMP
+	id SERIAL PRIMARY KEY,
+	referral_code VARCHAR(15) UNIQUE,
+	admin INTEGER REFERENCES users (id) NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	challenge_deadline TIMESTAMP
 );
