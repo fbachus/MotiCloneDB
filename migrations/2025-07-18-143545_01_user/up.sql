5180153718b58b51074aee46114d566693fe0c30
fbachus <felix.bachstein@gmail.com>
2025-07-22 02:10:25 +0200

    fix(sql): remove unnecessary nullable in user schema

--- migrations/2025-07-18-143545_01_user/up.sql
+++ migrations/2025-07-18-143545_01_user/up.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS "User" (
 	username VARCHAR(30) NOT NULL,
 	email VARCHAR(60) UNIQUE NOT NULL,
 	password VARCHAR(82) NOT NULL,
-	weekly_workout_goal INTEGER
+	salt VARCHAR(50) NOT NULL,
+	weekly_workout_goal INTEGER NOT NULL DEFAULT '0'
 );
