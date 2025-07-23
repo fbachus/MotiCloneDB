6bdee823cc216bfde56c0e0b2a522c2190b7198e
fbachus <felix.bachstein@gmail.com>
2025-07-23 02:09:20 +0200

    feat(backend): at least start a server

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-//use crate::model;
+use axum::{Router, routing::get};
+use axum::{http::StatusCode, response::IntoResponse};
+use tokio::net::TcpListener;
 
 pub mod controller;
 pub mod model;
 pub mod schema;
 
-fn main() {
-    println!("Hello, world!");
+async fn index() -> impl IntoResponse {
+    (StatusCode::OK, "Homepage")
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(index));
+    let listener = TcpListener::bind("0.0.0.0:4000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
